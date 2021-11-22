5f75a74a25e03a444b4144a44306999df8c1f386
magotts <techybet@gmail.com>
2021-11-22 02:53:05 -0500

    ...

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,10 +11,12 @@ class Quiz extends Component {
     this.state = {quiz_position: 1};
   }
   render () {
+    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
     return (
-      
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-   
+      <div>
+        {isQuizEnd ? <QuizEnd /> : 
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
+      </div>
     )
   }
 
