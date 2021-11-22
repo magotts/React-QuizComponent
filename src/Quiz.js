17bf1df4d5ff94eee13d361fbfb9e75a1a3c95fe
magotts <techybet@gmail.com>
2021-11-22 10:52:42 -0500

    ...

--- src/Quiz.js
+++ src/Quiz.js
@@ -26,7 +26,7 @@ class Quiz extends Component {
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
     return (
       <div>
-        {isQuizEnd ? <QuizEnd resetClickHandler={this.handlerResetClick.bind(this)}/> : 
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : 
       <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/> }
       </div>
     )
