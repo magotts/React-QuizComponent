36451c457bf3cef90359aef56cc25fe7cd26eb55
magotts <techybet@gmail.com>
2021-11-22 02:33:34 -0500

    add question and answer

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
   }
   render () {
     return (
-      <QuizQuestion />
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
       // {quizData.quiz_questions[0].instruction_text}
     )
   }
